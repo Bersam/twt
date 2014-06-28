38eb4e63e0b34f920f7f137ee53ad96fb59eea0f
Bersam Karbasion <bersam.k@gmail.com>
2014-06-29 02:10:06 +0430

    hmmm, written shitty but at least, working.

--- twt.sh
+++ twt.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
 # Set url here, result should be in csv format.
-url="http://payment.zconf.ir?now=1";
+url="http://example.com?now=1";
 
 # Sample: text="bla bla bla bla bla %s bla bla lba";
-# %s is content of $url;
+# Use %s for strings and %d for digits.
+# You should pass arguments when parsing CSV.
+
+text=()
 text[0]="bal bla bla %s bla";
 text[1]="%s foo foo foo foo";
 
-wget "http://linuxmag.ir/?now=1" -O current -q;
-current=$(echo current| xargs md5sum);
-current=(${current// / });
-old=$(echo old | xargs md5sum);
-old=(${old// / });
+count=${#text[@]};
 
-#printf $(echo text[0]) $current;
-printf $text $current;
-#if [ "${old}" != "${current}" ]
-#then
-#    t update "درصد پیشرفت: $(cat current)% | همین حالا به مشترکین نشریه‌ی #linuxmagir بپیوندید! http://linuxmag.ir";
-#fi
+# Get Url, This Url should be in csv format
+wget $url -O current -q;
+
+# Get MD5 files,
+current_MD5=$(echo current| xargs md5sum);
+old_MD5=$(echo old | xargs md5sum);
+
+# Trim
+old_MD5=(${old_MD5// / });
+current_MD5=(${current_MD5// / });
+
+# Generate a random number to show everytime different text;
+rand=$((RANDOM%$count))
+
+# These lines should be changed due your csv generated file
+if [ "${old}" != "${current}" ] then
+    while read arg1 do
+        t update "${text[$rand]}" $arg1;
+    done < "current"
+fi
+
 mv current old
