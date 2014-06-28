b6d18592d8c0ec8f43d7861f7cf811442a6b5a38
Bersam Karbasion <bersam.k@gmail.com>
2014-06-29 01:14:39 +0430

    hmmm, using printf to print formated strings

--- twt.sh
+++ twt.sh
@@ -5,22 +5,19 @@ url="http://payment.zconf.ir?now=1";
 
 # Sample: text="bla bla bla bla bla %s bla bla lba";
 # %s is content of $url;
-text[0]="";
-text[1]="";
+text[0]="bal bla bla %s bla";
+text[1]="%s foo foo foo foo";
 
-while :
-do
-    wget "http://linuxmag.ir/?now=1" -O current -q;
-    current=(echo current | xargs md5sum);
-    current=(${current// / });
-    old=$(echo old | xargs md5sum);
-    old=(${old// / });
+wget "http://linuxmag.ir/?now=1" -O current -q;
+current=$(echo current| xargs md5sum);
+current=(${current// / });
+old=$(echo old | xargs md5sum);
+old=(${old// / });
 
-    if [ "${old}" != "${current}" ]
-    then
-        t update "درصد پیشرفت: $(cat current)% | همین حالا به مشترکین نشریه‌ی #linuxmagir بپیوندید! http://linuxmag.ir";
-    fi
-    mv current old
-    sleep 345
-done
-
+#printf $(echo text[0]) $current;
+printf $text $current;
+#if [ "${old}" != "${current}" ]
+#then
+#    t update "درصد پیشرفت: $(cat current)% | همین حالا به مشترکین نشریه‌ی #linuxmagir بپیوندید! http://linuxmag.ir";
+#fi
+mv current old
